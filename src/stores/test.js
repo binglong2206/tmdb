6026fe9b2715b67893eeefa280910c57b3d92208
jason <binglong2206@gmail.com>
2022-09-10 16:59:45 +0700

    basic redux setup

---
+++ src/stores/test.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const testSlice = createSlice({
+  name: 'test',
+  initialState: {
+    test: 0,
+  },
+  reducers: {
+    testAction: (state, action) => {
+      state.test = action.payload
+    },
+  },
+})
+
+export const { testAction } = testSlice.actions
+
+export default testSlice.reducer
