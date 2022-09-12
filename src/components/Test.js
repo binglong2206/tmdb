ee29063a43570ae1435b98568ac3074c8dee4bb6
jason <binglong2206@gmail.com>
2022-09-13 04:50:18 +0700

    some default global css

--- src/components/Test.js
+++ src/components/Test.js
@@ -6,8 +6,20 @@ import { connect } from "react-redux";
 class NavBar extends React.Component {
   render(){
     return(
-      <section className='main-section'>
-    
+      <section class="gallery">
+        <div class="container">
+          <div class="main-wrapper">
+            <div class="titles">
+              <p class="subtitle">TMDB</p>
+              <h2 class="h2 title">Trending Movies:</h2>
+            </div>
+            <div class="tabs">
+                <button class="tabs-btn">Movies</button>
+                {/* <button class="tabs-btn">TV Shows</button> */}
+                <button class="tabs-btn">Favorites</button>
+            </div>
+          </div>
+        </div>
       </section>
       
     )
