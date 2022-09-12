4b4550e9925e5e05b5e630570aa25d29d1561b9d
jason <binglong2206@gmail.com>
2022-09-13 03:32:13 +0700

    another boiler test for gallery and tabs

--- src/components/Test.js
+++ src/components/Test.js
@@ -6,6 +6,21 @@ import { connect } from "react-redux";
 class NavBar extends React.Component {
   render(){
     return(
+      <section className='main-section'>
+        <div className='main-container'>
+          <div className='main-wrapper'>
+            <div className='main-title'>
+              <h1>title</h1>
+            </div>
+
+            <div className='main-tabs'>
+              <button className='tab-btn'>tab1</button>
+              <button className='tab-btn'>tab1</button>
+              <button className='tab-btn'>tab1</button>
+            </div>
+          </div>
+        </div>
+      </section>
       
     )
   }
