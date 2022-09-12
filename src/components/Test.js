f68b7493050251458a52a77520c1267585a0ad7c
jason <binglong2206@gmail.com>
2022-09-13 02:52:49 +0700

    new navbar boilerplate setup

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import '../styles/Test.css'
+import { mapState, mapDispatch } from "../stores/maps";
+import { connect } from "react-redux";
 
-class Test extends React.Component {
+class NavBar extends React.Component {
+  render(){
+    return(
+      <nav className="nav">
+        <div className="dropdown-wrapper">
+          <select>
+            <option>released year</option>
+          </select>
+          <select>
+            <option>types</option>
+          </select>
+        </div>
 
+        <div className="tabs">
+          <div>movies</div>
+          <div>tv shows</div>
+          <div>favorites</div>
+        </div>
+        
+      </nav>
+    )
+  }
 }
 
 
-export default Test;
+export default connect(mapState, mapDispatch)(NavBar);
