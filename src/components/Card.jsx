7bbb9e1c9549f3e1ef544036e7bf2391f49a6b86
jason <binglong2206@gmail.com>
2022-09-13 06:56:51 +0700

    new: bookmarks for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import '../styles/Card.css'
-import {BsBookmarkPlus} from 'react-icons/bs'
+import {BsBookmarkPlusFill, BsBookmarkDash, BsBookmarkPlus} from 'react-icons/bs'
 
 export default class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: false
+    }
+  }
+
+  hoverIn = () => {this.setState({isHover: true})}
+  hoverOut = () => {this.setState({isHover: false})}
+
 
   render() {
     const {title, release, lang, rating, source, mapKey} = this.props
 
     return (
-      <div className="poster-card" mapKey={mapKey}>
-          <div className="poster-banner">
-            <img
-              src={source}
-              alt={title + " poster"}
-            />
+      <div className="poster-card" mapKey={mapKey}
+        onMouseOver={this.hoverIn} onMouseLeave={this.hoverOut}
+        >
+            <div className="poster-banner">
+              <img
+                src={source}
+                alt={title + " poster"}
+              />
           </div>
 
         <div className="titles">
@@ -28,9 +40,11 @@ export default class Card extends React.Component {
           </div>
           <div>{lang}</div>
         </div>
-        <div className='badge'>
-          <BsBookmarkPlus color='blue' />
-        </div>
+        {this.state.isHover &&  
+        
+        <div className='badge badge-fadeIn'>
+          <BsBookmarkPlusFill size='50px' color='black' />
+        </div>}
       </div>
     );
   }
