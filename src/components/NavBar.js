78d3a5a5e010eb355d284f39a4016ddbb5172b9a
jason <binglong2206@gmail.com>
2022-09-12 15:04:07 +0700

    searchbar container setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,6 +26,25 @@ const {search} = this.state
           </div>
         </nav>
 
+        <div class={`search-container ${!search && 'hide'}`}>
+          <div class="link-search"></div>
+          <div class="search-bar">
+            <div className="input-container">
+              <input type="text" placeholder="Search for your favorite movies" />
+            </div>
+          </div>
+          <div class="link-close" onClick={()=>this.setState({search:false})} />
+
+          <div class="quick-links">
+            <h2>Trending keyword</h2>
+            <div className='links-container'>
+              <div onClick={()=>this.setState({search: false})}>keyword1</div>
+              <div onClick={()=>this.setState({search: false})}>keyword2</div>
+            </div>
+          
+          </div>
+        </div>
+
       </div>
     <div class={`overlay ${search && 'show'}`} onClick={()=>this.setState({search:false})}></div>
     </>
