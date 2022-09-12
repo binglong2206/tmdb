b64d468942ed3ba2c8b975f77244ac43e39899f8
jason <binglong2206@gmail.com>
2022-09-13 03:24:50 +0700

    restart again...

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,28 +1,12 @@
 import React from 'react'
-import '../styles/Test.css'
+import '../styles/NavBar.css'
 import { mapState, mapDispatch } from "../stores/maps";
 import { connect } from "react-redux";
 
 class NavBar extends React.Component {
   render(){
     return(
-      <nav className="nav">
-        <div className="dropdown-wrapper">
-          <select>
-            <option>released year</option>
-          </select>
-          <select>
-            <option>types</option>
-          </select>
-        </div>
-
-        <div className="tabs">
-          <div>movies</div>
-          <div>tv shows</div>
-          <div>favorites</div>
-        </div>
-        
-      </nav>
+      
     )
   }
 }
